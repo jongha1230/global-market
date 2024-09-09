e5c1e78765e3398a189b42b9c62373792c503cea
Jongha <jongha1230@gmail.com>
2024-09-10 01:53:08 +0900

    script: jest 초기 세팅

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  },
+};
+
+export default config;
